50ea17cf3f797b0c8aaee969ab45d84e4213bba5
aryan2609progress <aryan2609xcv@gmail.com>
2026-07-12 14:25:31 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.32%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> freq(128, 0);
+
+        for (char ch : s)
+            freq[ch]++;
+
+        int ans = 0;
+
+        for (int f : freq)
+            ans += (f / 2) * 2;
+
+        if (ans < s.size())
+            ans++;
+
+        return ans;
+    }
+};

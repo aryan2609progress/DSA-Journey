3e98aac71a6c96b84302d60c50d9e779fbb2ebc5
aryan2609progress <aryan2609xcv@gmail.com>
2026-08-18 12:35:10 +0530

    Time: 0 ms (100%), Space: 12.4 MB (14.6%) - LeetHub

---
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        unordered_set<int> s;
+
+        for (auto &r : ranges) {
+            for (int i = r[0]; i <= r[1]; i++)
+                s.insert(i);
+        }
+
+        for (int i = left; i <= right; i++) {
+            if (s.find(i) == s.end())
+                return false;
+        }
+
+        return true;
+    }
+};

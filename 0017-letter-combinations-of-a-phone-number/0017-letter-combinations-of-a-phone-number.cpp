8b358a38b9e0545accef910dc40ea786e23e77de
aryan2609progress <aryan2609xcv@gmail.com>
2026-09-18 22:06:21 +0530

    Time: 0 ms (100%), Space: 9.4 MB (27.95%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    vector<string> mp = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    void solve(string &digits, int index, string &current) {
+        if (index == digits.size()) {
+            ans.push_back(current);
+            return;
+        }
+
+        string letters = mp[digits[index] - '0'];
+
+        for (char ch : letters) {
+            current.push_back(ch);
+            solve(digits, index + 1, current);
+            current.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        string current;
+        solve(digits, 0, current);
+        return ans;
+    }
+};
